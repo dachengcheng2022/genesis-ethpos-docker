63a7c6b14d1208242850c8f29b5dd34162505527
qweda <1029842247@qq.com>
2023-11-05 17:05:30 +0800

    modify

--- clean.sh
+++ clean.sh
@@ -1,8 +1,5 @@
-docker rm -f $(docker ps -a -q) || true
-docker rmi block.com/geth || true
-docker rmi block.com/beacon-chain || true
-docker compose down || true
+docker-compose down
 rm -Rf ./consensus/ || true
 rm -Rf ./execution/ || true
-mkdir consensus
-mkdir execution
+rm -Rf ./consensus-data/ || true
+rm -Rf ./execution-data/ || true
--- execution-docker/resources/start_eth.sh
+++ execution-docker/resources/start_eth.sh
@@ -3,9 +3,11 @@ set -e
 
 if [ ! -e "${DATA_DIR}/geth" ]; then
   #config
-  # rm -rf ${DATA_DIR}/*
   geth --datadir ${DATA_DIR} init /execution/genesis.json
   cp /etc/jwtsecret ${DATA_DIR}
+  cp /etc/UTC--2022-08-19T17-38-31.257380510Z--123463a4b065722e99115d6c222f267d9cabb524 ${DATA_DIR}/keystore/
+  cp /etc/UTC--2023-08-19T04-56-23.233940000Z--630486d226bd0cc219623c79b54b9f8d01d1b95c ${DATA_DIR}/keystore/
+  cp /etc/UTC--2023-08-19T06-43-03.349924000Z--31dac3e0045b99d519d55644a3070bc7173af471 ${DATA_DIR}/keystore/
   cp /etc/geth_password.txt ${DATA_DIR}/
 fi
 
