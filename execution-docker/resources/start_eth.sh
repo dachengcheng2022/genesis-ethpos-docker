58c90c7875386e73d8a268b39a196c546bd914b6
“popeye2022” <popeye.li@poloniex.com>
2023-11-24 23:18:06 +0800

    modify eth port

--- execution-docker/resources/start_eth.sh
+++ execution-docker/resources/start_eth.sh
@@ -8,8 +8,8 @@ cp /etc/jwtsecret ${CONFIG_BASE_DIR}
 geth  --datadir ${DATA_DIR} --http --http.api=net,web3,eth,debug,engine,admin \
                      --http.corsdomain=* --http.vhosts=* --http.addr=0.0.0.0   \
                      --syncmode=full --networkid=97823 --nodiscover \
-                     --authrpc.jwtsecret=${CONFIG_BASE_DIR}/jwtsecret --authrpc.addr=0.0.0.0 --authrpc.port=${PORT_RPC} --authrpc.vhosts=* \
+                     --authrpc.jwtsecret=${CONFIG_BASE_DIR}/jwtsecret --authrpc.addr=0.0.0.0 --authrpc.port=8551 --authrpc.vhosts=* \
                      --allow-insecure-unlock  --password=${CONFIG_BASE_DIR}/geth_password.txt \
                      --verbosity=5 --ipcpath=/data-ephemera --nodekey=${CONFIG_BASE_DIR}/enode.key --nat=extip:${EXTIP}
-                     
+
 echo "geth starting endding "
