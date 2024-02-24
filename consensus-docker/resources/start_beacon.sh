2ec3d85c90fac0e6eca86b23a2e9e740eac6e4d9
dachengcheng022 <1029842247@qq.com>
2024-02-24 22:24:42 +0800

    init

---
+++ consensus-docker/resources/start_beacon.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+echo "beacon starting "
+
+beacon-chain \
+  --datadir=${DATA_DIR} \
+  --min-sync-peers=0 \
+  --genesis-state=${CONFIG_BASE_DIR}/genesis.ssz \
+  --chain-config-file=/config.yml \
+  --config-file=/config.yml \
+  --chain-id=97823 \
+  --rpc-host=0.0.0.0 \
+  --contract-deployment-block=0 \
+  --grpc-gateway-host=0.0.0.0 \
+  --monitoring-host=0.0.0.0 \
+  --execution-endpoint=http://eth:8551 \
+  --accept-terms-of-use \
+  --jwt-secret=${CONFIG_BASE_DIR}/jwtsecret \
+  --contract-deployment-block=0 \
+  --verbosity=info \
+  --p2p-local-ip=0.0.0.0 \
+  --p2p-host-ip=${HOST_IP} \
+  --p2p-static-id  \
+  --peer=${PEER_INFO} \
+  --pprof \
+  --pprofaddr=0.0.0.0
+
+echo "beacon starting endding "
+
+
---
+++ execution-docker/resources/start_eth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "geth starting "
+cp /etc/jwtsecret ${CONFIG_BASE_DIR}
+
+geth  --datadir ${DATA_DIR} --http --http.api=net,web3,eth,debug,engine,admin \
+                     --http.corsdomain=* --http.vhosts=* --http.addr=0.0.0.0   \
+                     --syncmode=full --networkid=97823 --nodiscover \
+                     --authrpc.jwtsecret=${CONFIG_BASE_DIR}/jwtsecret --authrpc.addr=0.0.0.0 --authrpc.port=8551 --authrpc.vhosts=* \
+                     --verbosity=4 --ipcpath=/data-ephemera --nodekey=${CONFIG_BASE_DIR}/enode.key --nat=extip:${EXTIP}
+
+echo "geth starting endding "
