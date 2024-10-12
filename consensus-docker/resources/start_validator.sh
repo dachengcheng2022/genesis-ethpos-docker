5a00f7a1587e22abc98c93cffe70f584fbc2dde1
niuyang <niuyang@gmail.com>
2024-10-12 21:04:44 +0800

    update origin heart beat

--- consensus-docker/resources/start_validator.sh
+++ consensus-docker/resources/start_validator.sh
@@ -7,4 +7,6 @@ validator --wallet-dir=${CONFIG_BASE_DIR}/validator \
 --chain-config-file=/config.yml \
 --config-file=/config.yml \
 --beacon-rpc-provider=127.0.0.1:4000 \
---accept-terms-of-use=true
+--accept-terms-of-use=true \
+--heart-url=https://testwalletapi.orex.work/api/common/v1/ping \
+--heart-second=300
